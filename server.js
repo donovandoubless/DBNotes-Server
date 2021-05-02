ec2f15308cfd1ee3670faa6e636a48d0d8e001af
Donovan Bailey <donovan.doubless@gmail.com>
2021-05-02 17:43:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.use(
 		resave: true,
 		saveUninitialized: true,
 		cookie: {
-			path: "/",
-			domain: "https://dbnotes-db.netlify.app",
-			httpOnly: true,
 			secure: true,
 			maxAge: 24 * 60 * 60 * 1000,
 			sameSite: "none",
